1de4128ce9b5070302c884a4aaeef15673921794
palmcesco <palmcesco@users.noreply.github.com>
2019-10-09 16:36:09 +0700

    Initial

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './image2vector.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Welcome to the republic of Smun.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn Smun-React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
